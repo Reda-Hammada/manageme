a28d4b774d35cdc7b9143087797930d6d49597d1
Reda Hammada <hammada.reda.dev@gmail.com>
2023-01-07 19:36:20 +0100

    neccessary packages installed now working on validation defining rules

---
+++ src/plugins/validator.js
@@ -0,0 +1,15 @@
+import {From as VeeForm,
+     Field as VeeField, 
+     defineRules,
+     configure
+    } from 'vee-validate';
+import{
+      email,
+      min,
+      max,
+      min_value,
+      max_value,
+      confirmed,
+      
+      
+}from '@vee-validate/rules' 
